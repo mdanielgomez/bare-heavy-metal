9b8d948831906a4f0eecd69589d1f9c6e8e46d4a
mdanielgomez <martindgomez95@gmail.com>
2026-02-03 23:03:21 -0500

    Add main function to turn on LED

--- src/main.c
+++ src/main.c
@@ -1,3 +1,20 @@
-#include "stm32f103xb.h"
+#include "stm32f103_regs.h"
 
-int main(void) {}
+int main(void)
+{
+    // Enable GPIO A Peripheral Clock
+    RCC->APB2ENR |= 1 << 2;
+
+    // Configure GPIOA5
+    // Reset PA5
+    GPIOA->CRL &= 4 << 20;
+
+    // Set PA5 configuration to open-drain
+    GPIOA->CRL |= 1 << 20;
+
+    // Set PA5 mode to output 2 MHz max
+    GPIOA->CRL |= 2 << 22;
+
+    // Turn on LED
+    GPIOA->BRR |= 1 << 5;
+}

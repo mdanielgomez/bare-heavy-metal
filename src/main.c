d2e6332a5bf569975dee3ed39ef6d742a8e9fa54
mdanielgomez <martindgomez95@gmail.com>
2026-02-08 01:53:12 -0500

    Change for initial debugging experience. Reset entire CRL register. Set to push pull. Use ODR instead of BRR. Add missing return.

--- src/main.c
+++ src/main.c
@@ -2,19 +2,20 @@
 
 int main(void)
 {
+
     // Enable GPIO A Peripheral Clock
     RCC->APB2ENR |= 1 << 2;
 
     // Configure GPIOA5
     // Reset PA5
-    GPIOA->CRL &= 4 << 20;
+    GPIOA->CRL = 0x44444444;
 
     // Set PA5 configuration to open-drain
-    GPIOA->CRL |= 1 << 20;
-
     // Set PA5 mode to output 2 MHz max
-    GPIOA->CRL |= 2 << 22;
+    GPIOA->CRL &= ~(0xF << 20);
+    GPIOA->CRL |= 0b0010 << 20;
 
     // Turn on LED
-    GPIOA->BRR |= 1 << 5;
+    GPIOA->ODR = 1 << 5;
+    return 0;
 }

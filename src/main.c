1fec7fed4d349396996a0b268d52a587a477721e
mdanielgomez <martindgomez95@gmail.com>
2026-03-14 19:31:58 -0400

    Add LED toggle using TIM2

--- src/main.c
+++ src/main.c
@@ -2,9 +2,26 @@
 
 int main(void)
 {
+    // Enable TIM2 Peripheral
+    RCC->APB1ENR |= 1 << 0;
+
+    // Disable the timer
+    TIM2->CR1 &= ~1;
+
+    // Set the prescalar and auto reload
+    TIM2->PSC = 8000u - 1u; // 8MHz / 8000 = 1 kHz (1 ms per tick)
+    TIM2->ARR = 500u - 1u;  // 500 ms per toggle -> 1 Hz blink
+    TIM2->EGR = 1u;
+    TIM2->SR  = 0u;
+
+    // Enable the timer
+    TIM2->CR1 |= 1;
 
     // Enable GPIO A Peripheral Clock
     RCC->APB2ENR |= 1 << 2;
+
+    // Enable the clock for TIM2
+    RCC->APB1ENR |= 1;
 
     // Configure GPIOA5
     // Reset PA5
@@ -15,7 +32,14 @@ int main(void)
     GPIOA->CRL &= ~(0xF << 20);
     GPIOA->CRL |= 0b0010 << 20;
 
-    // Turn on LED
-    GPIOA->BSRR = 1 << 5;
+    // Turn on LED with timer
+    while (1)
+    {
+        while ((TIM2->SR & 1u) == 0)
+        {
+        }
+        TIM2->SR &= ~1u;
+        GPIOA->ODR ^= (1 << 5);
+    }
     return 0;
 }

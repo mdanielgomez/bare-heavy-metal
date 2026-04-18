7b75fa0d05969c8f5537cb443f51cd96427cf84d
mdanielgomez <martindgomez95@gmail.com>
2026-04-18 17:20:23 -0400

    Add VTOR definition for reassigning vector table location

--- common/inc/arm.h
+++ common/inc/arm.h
@@ -1,4 +1,5 @@
 #define NVIC_BASE 0xE000E100
+#define VTOR_REG 0xE000ED08
 
 typedef struct
 {
@@ -8,3 +9,4 @@ typedef struct
 } NVIC_TypeDef;
 
 #define NVIC ((NVIC_TypeDef*)NVIC_BASE)
+#define VTOR (*(volatile unsigned int*)VTOR_REG)

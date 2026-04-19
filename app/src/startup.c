0e31f92041cb75e8d37328c54e6e44d6516059a4
mdanielgomez <martindgomez95@gmail.com>
2026-04-18 20:16:41 -0400

    Add startup code for app

---
+++ app/src/startup.c
@@ -0,0 +1,118 @@
+#include <stdint.h>
+
+// linker symbols
+extern uint32_t _estack;
+extern uint32_t _sidata, _sdata, _edata;
+extern uint32_t _sbss, _ebss;
+
+extern int main();
+
+void Reset_Handler(void);
+
+__attribute__((noreturn)) void Default_Handler(void)
+{
+    for (;;)
+    {
+    }
+}
+
+/* Cortex-M core handlers */
+void NMI_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void HardFault_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void MemManage_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void BusFault_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void UsageFault_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void SVC_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void DebugMon_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void PendSV_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void SysTick_Handler(void) __attribute__((weak, alias("Default_Handler")));
+
+/* Peripheral interrupts (STM32F103 subset) */
+void WWDG_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void PVD_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void TAMPER_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void RTC_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void FLASH_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void RCC_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void EXTI0_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void EXTI1_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void EXTI2_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void EXTI3_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void EXTI4_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel1_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel2_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel3_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel4_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel5_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel6_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel7_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void ADC1_2_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void USB_HP_CAN_TX_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void USB_LP_CAN_RX0_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void CAN_RX1_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void CAN_SCE_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void EXTI9_5_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void TIM1_BRK_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void TIM1_UP_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void TIM1_TRG_COM_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void TIM1_CC_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void TIM2_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void TIM3_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void TIM4_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void I2C1_EV_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void I2C1_ER_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void I2C2_EV_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void I2C2_ER_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void SPI1_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void SPI2_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void USART1_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void USART2_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void USART3_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void EXTI15_10_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void RTCAlarm_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void USBWakeUp_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+
+// vector table
+__attribute__((section(".isr_vector"))) void (*const vector_table[])(void) = {
+    (void (*)(void))&_estack, // Initial stack pointer
+    Reset_Handler,
+
+    NMI_Handler, HardFault_Handler, MemManage_Handler, BusFault_Handler, UsageFault_Handler, 0, 0,
+    0, 0, // Reserved
+    SVC_Handler, DebugMon_Handler, 0, PendSV_Handler, SysTick_Handler,
+
+    /* External interrupts */
+    WWDG_IRQHandler, PVD_IRQHandler, TAMPER_IRQHandler, RTC_IRQHandler, FLASH_IRQHandler,
+    RCC_IRQHandler, EXTI0_IRQHandler, EXTI1_IRQHandler, EXTI2_IRQHandler, EXTI3_IRQHandler,
+    EXTI4_IRQHandler, DMA1_Channel1_IRQHandler, DMA1_Channel2_IRQHandler, DMA1_Channel3_IRQHandler,
+    DMA1_Channel4_IRQHandler, DMA1_Channel5_IRQHandler, DMA1_Channel6_IRQHandler,
+    DMA1_Channel7_IRQHandler, ADC1_2_IRQHandler, USB_HP_CAN_TX_IRQHandler,
+    USB_LP_CAN_RX0_IRQHandler, CAN_RX1_IRQHandler, CAN_SCE_IRQHandler, EXTI9_5_IRQHandler,
+    TIM1_BRK_IRQHandler, TIM1_UP_IRQHandler, TIM1_TRG_COM_IRQHandler, TIM1_CC_IRQHandler,
+    TIM2_IRQHandler, TIM3_IRQHandler, TIM4_IRQHandler, I2C1_EV_IRQHandler, I2C1_ER_IRQHandler,
+    I2C2_EV_IRQHandler, I2C2_ER_IRQHandler, SPI1_IRQHandler, SPI2_IRQHandler, USART1_IRQHandler,
+    USART2_IRQHandler, USART3_IRQHandler, EXTI15_10_IRQHandler, RTCAlarm_IRQHandler,
+    USBWakeUp_IRQHandler};
+
+void Reset_Handler(void)
+{
+    // Copy data
+    uint32_t* src = &_sidata;
+    uint32_t* dst = &_sdata;
+
+    while (dst < &_edata)
+    {
+        *dst++ = *src++;
+    }
+
+    // .bss
+    uint32_t* b = &_sbss;
+    for (b; b < &_ebss; ++b)
+    {
+        *b = 0;
+    }
+    main();
+    while (1)
+    {
+    };
+}

f83389967507446a3195c8c0e48dc0603a750309
Martin Gomez <90948645+mdanielgomez@users.noreply.github.com>
2026-05-19 22:44:48 -0400

    Fix the DMA setup using the incorrect channel

--- app/src/main.c
+++ app/src/main.c
@@ -39,20 +39,20 @@ int main(void)
     volatile uint8_t uart_rx_buffer[64];
 
     // Set up DMA
-    DMA1->CCR1 &= ~(1 << 0);
-    DMA1->CPAR1 = (uint32_t)&USART2->DR;
-    DMA1->CMAR1 = (uint32_t)uart_rx_buffer;
-    DMA1->CNDTR1 = UART_RX_BUFFER_SIZE;
+    DMA1->CCR6 &= ~(1 << 0);
+    DMA1->CPAR6 = (uint32_t)&USART2->DR;
+    DMA1->CMAR6 = (uint32_t)uart_rx_buffer;
+    DMA1->CNDTR6 = UART_RX_BUFFER_SIZE;
 
-    DMA1->CCR1 = 0;
-    DMA1->CCR1 |= (1 << 7);
-    DMA1->CCR1 |= (1 << 5);
+    DMA1->CCR6 = 0;
+    DMA1->CCR6 |= (1 << 7);
+    DMA1->CCR6 |= (1 << 5);
 
     // Enable DMA on USART2
     USART2->CR3 |= (1 << 6);
 
     // Enable DMA
-    DMA1->CCR1 |= 1;
+    DMA1->CCR6 |= 1;
 
     // Disable the timer
     TIM2->CR1 &= ~1;

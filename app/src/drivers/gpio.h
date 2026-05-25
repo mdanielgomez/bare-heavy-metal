c0bed7e4f96215d8542659266f8a75aabcd3f237
Martin Gomez <90948645+mdanielgomez@users.noreply.github.com>
2026-05-25 17:25:01 -0400

    Add type def for cnf part of gpio register

--- app/src/drivers/gpio.h
+++ app/src/drivers/gpio.h
@@ -8,6 +8,18 @@ typedef enum {
   GPIO_MODE_OUTPUT_2MHZ = 2,
   GPIO_MODE_OUTPUT_10MHZ = 1,
   GPIO_MODE_OUTPUT_50MHZ = 3
-} gpi_mode_t;
+} gpio_mode_t;
+
+typedef enum {
+  GPIO_CNF_OUTPUT_PUSH_PULL = 0,
+  GPIO_CNF_OUTPUT_OPEN_DRAIN = 1,
+  GPIO_CNF_OUTPUT_AF_PUSH_PULL = 2,
+  GPIO_CNF_OUTPUT_AF_OPEN_DRAIN = 3,
+
+  GPIO_CNF_INPUT_ANALOG = 0,
+  GPIO_CNF_INPUT_FLOATING = 1,
+  GPIO_CNF_INPUT_PULL = 2
+} gpio_cnf_t;
+
 
 #endif

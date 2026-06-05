10ee573284e233e97f44c4d182f5b132b198a71d
mdanielgomez <martindgomez95@gmail.com>
2026-06-05 14:24:54 -0400

    Move RCC enums to rcc header

--- app/src/drivers/clock.c
+++ app/src/drivers/clock.c
@@ -10,22 +10,22 @@ void clock_init_72mhz(void)
     {
     }
 
-    FLASH_ACR &= ~0b111; // clear flash latency bits
-    FLASH_ACR |= 2 << 0; // set flash access latency to 2 wait cycles
-    FLASH_ACR |= 1 << 4; // enable prefetch buffer
+    FLASH_ACR &= ~0b111;               // clear flash latency bits
+    FLASH_ACR |= FLASH_LATENCY_2 << 0; // set flash access latency to 2 wait cycles
+    FLASH_ACR |= 1 << 4;               // enable prefetch buffer
 
     RCC->CFGR &= ~((0xF << 4) | (0x7 << 8) | (0x7 << 11) | (1 << 16) |
-                   (0xF << 18)); // clear bits, set ahb prescalr to no division
-    RCC->CFGR |= (0b100 << 8);   // set low speed prescalar to /2
-    RCC->CFGR |= (1 << 16);      // set hse as pll source
-    RCC->CFGR |= (0b0111 << 18); // set pll multiplication to x9
-    RCC->CR |= (1 << 24);        // enable PLL
+                   (0xF << 18));           // clear bits, set ahb prescalr to no division
+    RCC->CFGR |= (RCC_APB1PRE_DIV_2 << 8); // set low speed prescalar to /2
+    RCC->CFGR |= (1 << 16);                // set hse as pll source
+    RCC->CFGR |= (RCC_PLL_MULT_9 << 18);   // set pll multiplication to x9
+    RCC->CR |= (1 << 24);                  // enable PLL
     while (!(RCC->CR & (1 << 25)))
     {
     } // wait PLLRDY
 
-    RCC->CFGR &= ~(0b11 << 0); // clear bits
-    RCC->CFGR |= (0b10 << 0);  // set PLL as source for sysclock
+    RCC->CFGR &= ~(0b11 << 0);          // clear bits
+    RCC->CFGR |= (SYSCLK_SET_PLL << 0); // set PLL as source for sysclock
 
     while (((RCC->CFGR >> 2) & 0b11) != 0b10)
     {

47670069a8ace2fb6a47578694a3b8d56961449f
Martin Gomez <90948645+mdanielgomez@users.noreply.github.com>
2026-06-06 10:31:21 -0400

    Add init function that takes config as input

--- app/src/drivers/uart.h
+++ app/src/drivers/uart.h
@@ -2,6 +2,43 @@
 
 #include "stm32f103.h"
 
+typedef enum
+{
+    UART_STOP_1    = 0b00,
+    UART_STOP_0PT5 = 0b01,
+    UART_STOP_2    = 0b10,
+    UART_STOP_1PT5 = 0b11
+} uart_stop_t;
+
+typedef enum
+{
+    UART_PARITY_NONE,
+    UART_PARITY_EVEN,
+    UART_PARITY_ODD
+} uart_parity_t;
+
+typedef enum
+{
+    UART_WORD_8,
+    UART_WORD_9
+} uart_word_t;
+
+typedef enum
+{
+    UART_MODE_TX,
+    UART_MODE_RX,
+    UART_MODE_TX_RX
+} uart_mode_t;
+
+typedef struct
+{
+    uint32_t      baud_rate;
+    uart_stop_t   stop_bits;
+    uart_parity_t parity;
+    uart_word_t   word_length;
+    uart_mode_t   mode;
+} uart_config_t;
+
 void uart_set_baud_rate(USART_TypeDef* uart, uint32_t brr);
 void uart_enable_transmit(USART_TypeDef* uart);
 void uart_enable_receiver(USART_TypeDef* uart);
@@ -9,3 +46,4 @@ void uart_enable_uart(USART_TypeDef* uart);
 void uart_enable_dma(USART_TypeDef* uart);
 
 void uart_write_char(USART_TypeDef* uart, char c);
+void uart_init(USART_TypeDef* uart, uart_config_t config);
